25984a2154a2f91d90025e08ed4b76a608c3b2b1
CentOS 64 <hadoop@localhost.localdomain>
2017-03-18 11:54:03 -0700

    mysql

--- sql/basic.sql
+++ sql/basic.sql
@@ -14,6 +14,13 @@ rename database samp_db to mydatabase;//5.1.23版本后丢弃
 use samp_db;
 
 编码集
+show variables like 'character%' 
+– character_set_server：默认的内部操作字符集
+– character_set_client：客户端来源数据使用的字符集
+– character_set_connection：连接层字符集
+– character_set_results：查询结果字符集
+– character_set_database：当前选中数据库的默认字符集
+– character_set_system：系统元数据(字段名等)字符集
 
 7.查看所有表
 show tables;
@@ -23,6 +30,9 @@ desc samp_table;
 show create table samp_table;
 9.创建表
 create table if not exists samp_table (id int primary key,name varchar(20));
+
+mysql 数据类型
+
 11.删除表
 drop table samp_table
 12.修改表
@@ -31,9 +41,6 @@ change column name name varchar(40),
 add column gender enum('male','female');
 
 rename table samp_table to my_table;
-
-mysql 数据类型
-
 13.插入记录
 insert into my_table values(1,'001','male');
 insert into my_table (name,gender) values('002','female'),('003','female');
@@ -42,28 +49,59 @@ delete from my_table where name = '001';
 15.修改记录
 update my_table set gender='male' where table_id>6;
 16.查询记录
-select table_id,name from my_table order by name;//排序
 select * from my_table limit 0,10;
-
-
-添加索引
-添加主键
-...
-
-
-
-查询记录
-
 条件查询
-
+select * from stu where id =1;
 分组查询
-
+select cid,count(cid) from stu group by cid with rollup;
+select cid,count(cid) from stu group by cid with rollup having cid is not NULL;
+排序
+select table_id,name from my_table order by name asc;//default
+select table_id,name from my_table order by name desc;
 关联查询 左右内外
+inner join 
+select stu.*,cla.* from stu join cla on stu.cid=cla.id;
+select stu.*,cla.* from stu inner join cla on stu.cid=cla.id;
+
+outer join  
+	right join
+	select stu.*,cla.* from stu right outer join cla on stu.cid=cla.id;
+	select stu.*,cla.* from stu right join cla on stu.cid=cla.id;
+	left join
+	select stu.*,cla.* from stu left outer join cla on stu.cid=cla.id;
+	select stu.*,cla.* from stu left outer join cla on stu.cid=cla.id;
+	full join
+	select stu.*,cla.* from stu left outer join cla on stu.cid=cla.id union 
+	select stu.*,cla.* from stu right outer join cla on stu.cid=cla.id;//mysql not support full join
+cross join
+	select * from stu,cla;
+	select * from stu cross join cla;
+
+
+添加主键
+
+union
+select country from Websites
+union
+select country from apps;
+
+select country from Websites
+union all
+select country from apps;
+
+null
+is null
+is not null
+
+like
+select * from stu where name like '%rose' or 'rose%';
 
 主外键
 
 索引（唯一索引，组合索引）
+create index index1 on stu (id);
+alter table stu add index index_id(id);
+alter table stu drop index index1;
 
-引擎
-
-用户权限
+create unique index index1 on stu (id);
+alter table stu add unique index_id(id);
